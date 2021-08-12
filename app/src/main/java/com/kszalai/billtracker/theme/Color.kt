00bb113e6281adfc387d6b17e42afce38b400135
Kyle Szalai <kyleszalai@gmail.com>
2021-08-11 21:29:30 -0400

    Adding composable colors and theme
    Adding extension function to determine a background color to use based on payment date

--- app/src/main/java/com/kszalai/billtracker/theme/Color.kt
+++ app/src/main/java/com/kszalai/billtracker/theme/Color.kt
@@ -4,6 +4,14 @@ import androidx.compose.material.Colors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
+val colorPrimary = Color(0xff008577)
+val colorPrimaryDark = Color(0xff00574B)
+val colorPrimaryLight = Color(0xff4cb5a6)
+val colorAccent = Color(0xffD81B60)
+val colorAccentDark = Color(0xffa00037)
+val colorAccentLight = Color(0xffff5c8d)
+val textColorLight = Color(0xff333333)
+val textColorDark = Color(0xffffffff)
 val farOutLight = Color(0xff81c784)
 val dueThisWeekLight = Color(0xfffff176)
 val pastDueLight = Color(0xffe57373)
@@ -11,17 +19,14 @@ val farOutDark = Color(0xff003300)
 val dueThisWeekDark = Color(0xffbc5100)
 val pastDueDark = Color(0xff7f0000)
 
+val Colors.billTextColor : Color
+    @Composable get() = if (isLight) textColorDark else textColorLight
+
 val Colors.farOut : Color
-    @Composable get() = if (isLight) farOutLight else farOutDark
+    @Composable get() = if (isLight) farOutDark else farOutLight
 
 val Colors.dueThisWeek : Color
-    @Composable get() = if (isLight) dueThisWeekLight else dueThisWeekDark
+    @Composable get() = if (isLight) dueThisWeekDark else dueThisWeekLight
 
 val Colors.pastDue : Color
-    @Composable get() = if (isLight) pastDueLight else pastDueDark
-
-data class BillTrackerColors(
-    val farOut: Color,
-    val dueThisWeek: Color,
-    val pastDue: Color
-)
+    @Composable get() = if (isLight) pastDueDark else pastDueLight
--- app/src/main/java/com/kszalai/billtracker/ui/viewholders/BillComposable.kt
+++ app/src/main/java/com/kszalai/billtracker/ui/viewholders/BillComposable.kt
@@ -1,80 +1,90 @@
 package com.kszalai.billtracker.ui.viewholders
 
+import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Card
-import androidx.compose.material.Colors
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.kszalai.billtracker.helpers.determineComposableColorFromDate
 import com.kszalai.billtracker.helpers.formatToCurrency
 import com.kszalai.billtracker.helpers.getIcon
 import com.kszalai.billtracker.models.BillObject
 import com.kszalai.billtracker.models.BillPayment
 import com.kszalai.billtracker.models.BillType
 import com.kszalai.billtracker.models.CreditCardLimit
+import com.kszalai.billtracker.theme.BillTrackerTheme
+import com.kszalai.billtracker.theme.billTextColor
 
 @Composable
 fun BillComposable(
     data: BillObject
 ) {
-    Card(
-        backgroundColor = Color.White
-    ) {
-        Column(
-            modifier = Modifier
-                .padding(
-                    all = 20.dp
-                )
+    BillTrackerTheme {
+        Card(
+            backgroundColor = data.nextPayment.paymentDate.determineComposableColorFromDate()
         ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically
+            Column(
+                modifier = Modifier
+                    .padding(
+                        all = 20.dp
+                    )
             ) {
-                Image(
-                    painter = painterResource(id = data.billType.getIcon()),
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.width(16.dp))
-                Text(
-                    text = data.billName,
-                    fontSize = 20.sp
-                )
-            }
-            Spacer(modifier = Modifier.height(4.dp))
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-            ) {
-                Text(
-                    text = data.nextPayment.amount.formatToCurrency(),
-                    fontSize = 12.sp,
-                    fontStyle = FontStyle.Italic
-                )
-                Text(
-                    text = " - Due On ${data.nextPayment.paymentDate}",
-                    fontSize = 12.sp
-                )
-            }
-            data.balance?.let {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
+                    Image(
+                        painter = painterResource(id = data.billType.getIcon()),
+                        contentDescription = null
+                    )
+                    Spacer(modifier = Modifier.width(16.dp))
                     Text(
-                        text = "Balance:",
+                        text = data.billName,
+                        fontSize = 20.sp,
+                        color = MaterialTheme.colors.billTextColor
+                    )
+                }
+                Spacer(modifier = Modifier.height(4.dp))
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = data.nextPayment.amount.formatToCurrency(),
                         fontSize = 12.sp,
-                        fontStyle = FontStyle.Italic
+                        fontStyle = FontStyle.Italic,
+                        color = MaterialTheme.colors.billTextColor
                     )
-                    Spacer(modifier = Modifier.width(3.dp))
                     Text(
-                        text = data.balance.formatToCurrency(),
-                        fontSize = 12.sp
+                        text = " - Due On ${data.nextPayment.paymentDate}",
+                        fontSize = 12.sp,
+                        color = MaterialTheme.colors.billTextColor
                     )
+                }
+                data.balance?.let {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Text(
+                            text = "Balance:",
+                            fontSize = 12.sp,
+                            fontStyle = FontStyle.Italic,
+                            color = MaterialTheme.colors.billTextColor
+                        )
+                        Spacer(modifier = Modifier.width(3.dp))
+                        Text(
+                            text = data.balance.formatToCurrency(),
+                            fontSize = 12.sp,
+                            color = MaterialTheme.colors.billTextColor
+                        )
+                    }
                 }
             }
         }
@@ -82,13 +92,14 @@ fun BillComposable(
 }
 
 @Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
 @Composable
 fun PreviewBillComposable() {
     BillComposable(
         data = BillObject(
             billName = "Discover",
             nextPayment = BillPayment(
-                amount = 129.70,
+                amount = 150.70,
                 paymentDate = "06/06/2020"
             ),
             pastDue = 0.0,
